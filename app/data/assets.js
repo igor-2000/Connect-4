ea6d8cc6b7374f4aa80b3908a98946e42357acdc
Igor Ristov <77211522+igor-2000@users.noreply.github.com>
2024-11-11 14:45:21 +0100

    Add files via upload

---
+++ app/data/assets.js
@@ -0,0 +1,12 @@
+export default {
+  sprites: {
+    pathPrefix: './assets/',
+    PNGs: ['board', 'blue-coin', 'red-coin', 'blue-marker', 'red-marker', 'filled-blue-coin', 'filled-red-coin', 'btn', 'restart-btn', 'sound-on', 'sound-off'],
+    JPGs: [],
+  },
+
+  sounds: {
+    pathPrefix: './assets/sounds/',
+    list: ['btn', 'bgm', 'drop', 'win', 'fail'],
+  },
+};
---
+++ app/data/hole-states.js
@@ -0,0 +1,5 @@
+export default {
+    EMPTY: 0,
+    BLUE: 1,
+    RED: 2,
+}
